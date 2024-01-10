1dbb3314d3a554932efd4d43add7ca0d6ac9b5b0
NSANZIMURA <marcnsanzimfura20@gmail.com>
2024-01-10 10:43:47 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,10 @@
-earch_replace(my_list, search, replace):
-        new_list = []
-            for i in my_list:
-                        if i == search:
-                                        new_list.append(replace)
-                                                else:
-                                                                new_list.append(i)
-                                                                    return new_list
+
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
