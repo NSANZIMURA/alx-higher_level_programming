50b975827fc13d8f0e73ead71496c12a5edc060d
NSANZIMURA <marcnsanzimfura20@gmail.com>
2024-01-10 10:21:46 -0800

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,3 @@
+quare_matrix_simple(matrix=[]):
+        """A function that computes the square value. Use Map and Lambda"""
+            return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
