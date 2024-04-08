d4ada2ed0f5c1377722c4e770ff7c83e52967ebb
NSANZIMURA <marcnsanzimfura20@gmail.com>
2024-04-08 07:27:29 -0700

    103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
+'use strict';
+
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
 
-myObject.incr = function () {
-  this.value++;
+myObject.incr = () => {
+  myObject.value++;
 };
 
 myObject.incr();
 console.log(myObject);
-myObject.incr();
-console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
 
+myObject.incr();
+console.log(myObject);
