45fb199bbe15fe355be3f3f52b9dfe714d54d4c3
NSANZIMURA <marcnsanzimfura20@gmail.com>
2024-04-27 13:11:34 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""Python script Fetches https://intranet.hbtn.io/status."""
-if __name__ == "__main__":
-    import urllib.request
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
-            as response:
+
+def main():
+    """
+    Funtion to print a response of a specific url
+    """
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         html = response.read()
-
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+        print('\t- utf8 content: {}'.format(html.decode('utf8')))
+
+if __name__ == "__main__":
+    main()
 
